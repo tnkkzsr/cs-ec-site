08a866373adea9b97b43dca649a249c11ad609ff
kazuzuz <m.kazuki1021@gmail.com>
2024-02-23 05:15:32 +0900

    出品機能の追加

--- src/items/urls.py
+++ src/items/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'items'
 
 urlpatterns = [
-    path('add_item', views.Additem, name='add_item'),
+    path('add_item_page', views.Add_Item_page, name='add_item'),
+    path('add_item_completed', views.Add_Item_Completed, name='add_item_completed'),
 ]
--- src/items/views.py
+++ src/items/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render
+from .forms import Add_Item_Form
+from .models import Item
 
 
-def Additem(request):
-    # form = AddItemForm(request.POST)
+def Add_Item_page(request):
+    form = Add_Item_Form(request.POST)
+    return render(request, 'items/add_item.html', {'form': form})
+
+
+def Add_Item_Completed(request):
+    form = Add_Item_Form(request.POST)
     
-    # if request.method == "POST":
-    #         if form.is_valid():
-    #             item_title = form.cleaned_data['item_title']
-    #             item_explain = form.cleaned_data['item_explain']
-    #             item_category = form.cleaned_data['item_category']
-    #             item_condition = form.cleaned_data['item_condition']
-    #             item_price = form.cleaned_data['item_price']
-    #             seller = request.user
-    #             item, created = Item.objects.update_or_create(
-    #                 reviewer = reviewer,
-    #                 book = book,
-                    
-    #                 defaults={
-    #                     'book': book,'review_text': review_text,'score': score,'reviewer': reviewer,'review_title': review_title
-    #                 }
-    #             )
-    #             return HttpResponseRedirect(reverse("book:detail", args=[book_id]))
-    #         else:
-    #             pass
-            
-
-    return render(request, 'items/add_item.html')
+    if request.method == "POST":
+            if form.is_valid():
+                item_title = form.cleaned_data['item_title']
+                item_explain = form.cleaned_data['item_explain']
+                item_category = form.cleaned_data['item_category']
+                item_condition = form.cleaned_data['item_condition']
+                item_price = form.cleaned_data['item_price']
+                # seller = request.user
+                Item.objects.create(
+                    item_title=item_title,
+                    item_explain=item_explain,
+                    item_category=item_category,
+                    item_condition=item_condition,
+                    item_price=item_price,
+                    # seller=seller
+                )
+                return render(request, 'items/add_item_completed.html')
+            else:
+                pass
